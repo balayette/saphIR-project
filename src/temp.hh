68fdecf5aa42928ca77e45f34e626fca25233b16
Nicolas Manichon <nicolas.manichon@lse.epita.fr>
2019-11-29 21:22:39 +0100

    temp: Add std::hash<temp::label>

--- src/temp.hh
+++ src/temp.hh
@@ -12,12 +12,17 @@ struct base_temp {
 
       public:
 	base_temp() = delete;
+	bool operator==(const base_temp &rhs) const { return sym_ == rhs.sym_; }
+	bool operator!=(const base_temp &rhs) const
+	{
+		return !(sym_ == rhs.sym_);
+	}
 	symbol sym_;
 };
 
 struct temp : base_temp {
 	temp() : base_temp(unique_temp()) {}
-        temp(const std::string& name) : base_temp(name) {}
+	temp(const std::string &name) : base_temp(name) {}
 };
 
 struct label : base_temp {
@@ -29,4 +34,15 @@ inline std::ostream &operator<<(std::ostream &os, const base_temp &t)
 {
 	return os << t.sym_;
 }
+
 } // namespace temp
+
+namespace std
+{
+template <> struct hash<temp::label> {
+	std::size_t operator()(const temp::label &s) const
+	{
+		return std::hash<std::string>{}(s.sym_.get());
+	}
+};
+} // namespace std
