13bb998750dedbbfeef8e2fa4e50523a05a86150
Nicolas Manichon <nmanichon@gmail.com>
2019-12-08 21:31:11 +0100

    misc: Add cmpop inversion

--- include/frontend/ops.hh
+++ include/frontend/ops.hh
@@ -7,6 +7,8 @@ namespace ops
 enum class binop { MINUS, PLUS, MULT, DIV };
 enum class cmpop { EQ, NEQ };
 
+cmpop invert_cmpop(cmpop op);
+
 const std::string &binop_to_string(binop op);
 const std::string &cmpop_to_string(cmpop op);
 } // namespace ops
--- src/frontend/ops.cc
+++ src/frontend/ops.cc
@@ -5,6 +5,13 @@ namespace ops
 const std::string binop_str[] = {"-", "+", "*", "/"};
 const std::string cmpop_str[] = {"==", "!="};
 
+cmpop invert_cmpop(cmpop op)
+{
+	if (op == cmpop::EQ)
+		return cmpop::NEQ;
+	return cmpop::EQ;
+}
+
 const std::string &binop_to_string(binop op)
 {
 	return binop_str[static_cast<int>(op)];
@@ -14,4 +21,4 @@ const std::string &cmpop_to_string(cmpop op)
 {
 	return cmpop_str[static_cast<int>(op)];
 }
-} // namespace op
+} // namespace ops
