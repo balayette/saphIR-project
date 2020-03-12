e2cb4c046cc80db2a65aae9f49fa5c522a617559
Nicolas Manichon <nmanichon@gmail.com>
2020-03-12 12:40:07 +0100

    misc: Add compile.sh

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$#" -lt 3 ]; then
+        echo "usage: $0 input.jit out_asm.S out_binary [verbose]"
+        exit 1
+fi
+
+tmp=$(mktemp --suffix=.cpp.jit)
+cpp -ftrack-macro-expansion=0 -P "$1" > "$tmp"
+if [ "$#" -eq 4 ]; then
+        ./jit "$tmp" "$2"
+else
+        ./jit "$tmp" "$2" > /dev/null
+fi
+
+gcc "$2" -o "$3"
+
+echo "source: $1"
+echo "preprocessed: $tmp"
+echo "asm: $2"
+echo "binary: $3"
