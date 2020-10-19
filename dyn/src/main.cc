de058ee2170b2fb6ba0a8e079a95687e06801dd6
Nicolas Manichon <nmanichon@gmail.com>
2020-10-19 11:31:52 +0200

    dyn: Add argument parsing

--- dyn/src/main.cc
+++ dyn/src/main.cc
@@ -1,19 +1,57 @@
 #include <iostream>
+#include <getopt.h>
 #include "utils/fs.hh"
 #include "dyn/emu.hh"
 #include "dyn/unicorn-emu.hh"
+#include "fmt/format.h"
+#include <string>
+#include <optional>
+
+struct options {
+	std::optional<std::string> coverage_file;
+	std::string binary;
+	bool help;
+	bool singlestep;
+};
+
+options parse_options(int argc, char **argv)
+{
+	int opt = 0;
+	options ret{};
+
+	while ((opt = getopt(argc, argv, "shc:")) != -1 && !ret.help) {
+		if (opt == 'c')
+			ret.coverage_file = optarg;
+		else if (opt == 'h')
+			ret.help = true;
+		else if (opt == 's')
+			ret.singlestep = true;
+		else {
+			fmt::print("option '{}' not recognized\n", (char)opt);
+			ret.help = true;
+		}
+	}
+
+	if (optind == argc)
+		ret.help = true;
+	else
+		ret.binary = argv[optind];
+
+	return ret;
+}
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		std::cerr << "usage: dyn binary\n";
+	auto opts = parse_options(argc, argv);
+	if (opts.help) {
+		fmt::print("{} [-c coverage_output] [-s] [-h] binary\n",
+			   argv[0]);
 		return 1;
 	}
 
-	utils::mapped_file file(argv[1]);
-	// dyn::emu emu(file, dyn::emu_params(true, "lighthouse_out.txt"));
-	dyn::emu emu(file, dyn::emu_params(false));
-
+	utils::mapped_file file(opts.binary);
+	dyn::emu emu(file,
+		     dyn::emu_params(opts.singlestep, opts.coverage_file));
 	emu.setup();
 	emu.run();
 }
