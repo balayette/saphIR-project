e8ec93c55a491e93ae77a96ba28b1d6795f557e6
Nicolas Manichon <nmanichon@gmail.com>
2020-10-24 14:30:08 +0200

    fuzz: libmicrodns harness and dump the DB on exit

--- fuzz/src/fuzz.cc
+++ fuzz/src/fuzz.cc
@@ -39,6 +39,43 @@ class basic_harness : public fuzz::harness
 	uint64_t exit_addr_;
 };
 
+class microdns_harness : public fuzz::harness
+{
+      public:
+	microdns_harness(const elf::elf &bin) : fuzz::harness(bin)
+	{
+		parse_addr_ = bin_.symbol_by_name("mdns_parse")->address();
+		exit_addr_ = bin_.symbol_by_name("exit")->address();
+	}
+
+	virtual void setup(dyn::base_emu &emu) override
+	{
+		payload_addr_ = emu.alloc_mem(500);
+		emu.setup();
+	}
+
+	virtual uint64_t base_state_addr() const override
+	{
+		return parse_addr_;
+	}
+
+	virtual void case_setup(dyn::base_emu &emu, const char *data,
+				size_t sz) override
+	{
+		emu.mem_write(payload_addr_, data, sz);
+		emu.reg_write(mach::aarch64::regs::R2, payload_addr_);
+		emu.reg_write(mach::aarch64::regs::R3, sz);
+	}
+
+	virtual uint64_t fuzz_end_addr() const override { return exit_addr_; }
+
+      private:
+	uint64_t payload_addr_;
+
+	uint64_t parse_addr_;
+	uint64_t exit_addr_;
+};
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
@@ -48,16 +85,25 @@ int main(int argc, char *argv[])
 
 	fuzz::database db;
 
-	fuzz::input base_input(100);
-	base_input.data[0] = 0x45;
+	fuzz::input base_input(56);
 	db.add(base_input);
 
-	fuzz::mutator mutator(db, 100);
+	uint8_t buf[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+			 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x33,
+			 0x97, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,
+			 0x00, 0x00, 0xff, 0x00, 0x00, 0xff, 0x00, 0x00,
+			 0x00, 0x00, 0x13, 0x00, 0x79, 0x00, 0x00, 0x01,
+			 0x00, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+	std::memcpy(db[0].data.data(), buf, sizeof(buf));
+
+	fuzz::mutator mutator(db, 56);
 
 	utils::mapped_file file(argv[1]);
 	dyn::emu emu(file, dyn::emu_params(false));
 
-	basic_harness h(emu.bin());
+	microdns_harness h(emu.bin());
 
 	emu.init();
 	h.setup(emu);
@@ -110,18 +156,16 @@ int main(int argc, char *argv[])
 			bool added = false;
 			emu.add_on_entry_callback([&](auto pc) {
 				auto [_, ins] = bbs.insert(pc);
+				if (ins)
+					fmt::print("  New BB {:#x}\n", pc);
 				added |= ins;
 			});
 
 			emu.run_until(h.fuzz_end_addr());
 
 			if (added) {
-				fmt::print(
-					"[{}]: {:#x}{}{}{}{:#x} => Coverage {}\n",
-					i, input.data[0], (char)input.data[1],
-					(char)input.data[2],
-					(char)input.data[3], input.data[4],
-					bbs.size());
+				fmt::print("[{}]: Coverage {}\n", i,
+					   bbs.size());
 				db.add(input);
 			}
 		}
@@ -130,4 +174,9 @@ int main(int argc, char *argv[])
 	std::ofstream f("fuzzing_cov.txt");
 	for (const auto &v : bbs)
 		f << fmt::format("{:#x}\n", v);
+
+	for (size_t i = 0; i < db.size(); i++) {
+		std::ofstream inpt_f(fmt::format("output/{}.bin", i));
+		inpt_f << db[i].to_string() << '\n';
+	}
 }
